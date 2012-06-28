0a5d05953ad32e4409d36bc7ebfa06db277f2762
Ping Yu <ping@intridea.com>
2012-06-28 10:29:38 -0500

    overwritten active record migrator's run method, so the db schema migration versions are recorded properly for each sharding database. and this will allow migration to be run individual sharded database, instead of relying on the master database to provide migrated versions. and this should help us to add new sharded database and migrate them properly.

--- lib/octopus/migration.rb
+++ lib/octopus/migration.rb
@@ -1,69 +1,65 @@
-module Octopus::Migration
-  def self.extended(base)
-    class << base
-      def announce_with_octopus(message)
-        announce_without_octopus("#{message} - #{get_current_shard}")
-      end
-
-      alias_method_chain :migrate, :octopus
-      alias_method_chain :announce, :octopus
-      attr_accessor :current_shard
-    end
-  end
-
-  def self.included(base)
-    base.class_eval do
-      def announce_with_octopus(message)
-        announce_without_octopus("#{message} - #{get_current_shard}")
-      end
-
-      alias_method_chain :migrate, :octopus
-      alias_method_chain :announce, :octopus
-      attr_accessor :current_shard
-    end
-  end
-
-  def using(*args)
-    if self.connection().is_a?(Octopus::Proxy)
-      args.each do |shard|
-        self.connection().check_schema_migrations(shard)
-      end
-
-      self.connection().block = true
-      self.current_shard = args
-      self.connection().current_shard = args
-    end
-
-    return self
-  end
-
-  def using_group(*groups)
-    if self.connection.is_a?(Octopus::Proxy)
-      groups.each do |group|
-        shards = self.connection.shards_for_group(group) || []
-
-        shards.each do |shard|
-          self.connection.check_schema_migrations(shard)
+module Octopus
+  module Migration
+    def self.extended(base)
+      class << base
+        def announce_with_octopus(message)
+          announce_without_octopus("#{message} - #{get_current_shard}")
         end
-      end
 
-      self.connection.block = true
-      self.connection.current_group = groups
+        alias_method_chain :migrate, :octopus
+        alias_method_chain :announce, :octopus
+        attr_accessor :current_shard
+      end
     end
 
-    self
-  end
+    def self.included(base)
+      base.class_eval do
+        def announce_with_octopus(message)
+          announce_without_octopus("#{message} - #{get_current_shard}")
+        end
 
-  def get_current_shard
-    "Shard: #{ActiveRecord::Base.connection.current_shard()}" if ActiveRecord::Base.connection.respond_to?(:current_shard)
-  end
+        #alias_method_chain :migrate, :octopus
+        alias_method_chain :announce, :octopus
+        attr_accessor :current_shard
+      end
+    end
 
-  def migrate_with_octopus(direction)
-    conn = ActiveRecord::Base.connection
-    return migrate_without_octopus(direction) unless conn.is_a?(Octopus::Proxy)
-    self.connection().current_shard = self.current_shard if self.current_shard != nil
+    def using(*args)
+      if self.connection().is_a?(Octopus::Proxy)
+        args.each do |shard|
+          self.connection().check_schema_migrations(shard)
+        end
 
-    begin
+        self.connection().block = true
+        self.current_shard = args
+        self.connection().current_shard = args
+      end
+
+      return self
+    end
+
+    def using_group(*groups)
+      if self.connection.is_a?(Octopus::Proxy)
+        groups.each do |group|
+          shards = self.connection.shards_for_group(group) || []
+
+          shards.each do |shard|
+            self.connection.check_schema_migrations(shard)
+          end
+        end
+
+        self.connection.block = true
+        self.connection.current_group = groups
+      end
+
+      self
+    end
+
+    def get_current_shard
+      "Shard: #{ActiveRecord::Base.connection.current_shard()}" if ActiveRecord::Base.connection.respond_to?(:current_shard)
+    end
+    
+    def shards
       shards = Set.new
 
       if conn.current_group
@@ -74,22 +70,59 @@ module Octopus::Migration
       elsif conn.current_shard.is_a?(Array)
         shards.merge(conn.current_shard)
       end
-
-      if shards.any?
-        conn.send_queries_to_multiple_shards(shards.to_a) do
-          migrate_without_octopus(direction)
-        end
-      else
-        migrate_without_octopus(direction)
-      end
-    ensure
-      conn.clean_proxy
+      shards
     end
+    # def migrate_with_octopus(direction)
+    #   conn = ActiveRecord::Base.connection
+    #   return migrate_without_octopus(direction) unless conn.is_a?(Octopus::Proxy)
+    #   self.connection().current_shard = self.current_shard if self.current_shard != nil
+    # 
+    #   begin
+    #     if shards.any?
+    #       conn.send_queries_to_multiple_shards(shards.to_a) do
+    #         migrate_without_octopus(direction)
+    #       end
+    #     else
+    #       migrate_without_octopus(direction)
+    #     end
+    #   ensure
+    #     conn.clean_proxy
+    #   end
+    # end
+  end
+  module Migrator
+    def self.extended(base)
+    end
+
+    def self.included(base)
+      base.class_eval do
+        alias_method_chain :run, :octopus
+      end
+    end
+    def run_with_octopus
+      conn = ActiveRecord::Base.connection
+      return run_without_octopus unless conn.is_a?(Octopus::Proxy)            
+      target = migrations.detect { |m| m.version == @target_version }
+      raise UnknownMigrationVersionError.new(@target_version) if target.nil?
+      shards = target.respond_to? :shards ? target.shards : []
+      begin
+        if shards.any?
+          conn.send_queries_to_multiple_shards(shards.to_a) do
+            run_without_octopus(direction)
+          end
+        else
+          run_without_octopus(direction)
+        end
+      ensure
+        conn.clean_proxy
+      end      
+    end  
   end
 end
-
 if Octopus.rails31?
   ActiveRecord::Migration.send(:include, Octopus::Migration)
+  ActiveRecord::Migrator.send(:include, Octopus::Migrator)
 else
   ActiveRecord::Migration.extend(Octopus::Migration)
+  ActiveRecord::Migrator.extend(Octopus::Migrator)
 end
